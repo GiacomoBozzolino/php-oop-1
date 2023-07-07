cac7d515fd5ecef85aedad8ecb7f3d54c786d0c8
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-07 15:16:35 +0200

    [FEAT] creazione class Movie e prima istanza

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
+    class Movie {
+        public $title;
+        public $genre;
+        public $director;
+        public $starring;
+        public $date;
+        public $language;
+
+    }
+
+
+
+    $alien = new Movie();
+    $alien->title ="Alien";
+    $alien->genre ="Science fiction";
+    $alien->director ="Ridley Scott";
+    $alien->starring ="Sigourney Weaver";
+    $alien->date ="1979";
+    $alien->language ="English";
+
+    echo $alien->title;
+    
 ?>
