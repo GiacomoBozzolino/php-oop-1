9c3737e2918c3cecd2375204879141430db35648
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-10 09:44:59 +0200

    [FIX] aggiunto construct funzionante

--- Bonus/models/classes.php
+++ Bonus/models/classes.php
@@ -1,37 +1,37 @@
 <?php
-class Movie {
-    public $title;
-    public $genre;
-    public $director;
-    public $starring;
-    public $date;
-    public $language;
-    public $oscar;
-    public $poster;
+    class Movie {
+        public $title;
+        public $genre;
+        public $director;
+        public $starring;
+        public $date;
+        public $language;
+        public $oscar;
+        public $poster;
 
-    function __construct($title, $genre, $director, $starring, $date, $language, $oscar){
-        $this-> title= $title;
-        $this-> genre= $genre;
-        $this-> director= $director;
-        $this-> starring= $starring;
-        $this-> date= $date;
-        $this-> language= $language;
-        $this-> oscar= $oscar;
-    }    
-        
-    // funzione per attribuire il premio
-    public function setOscar ($oscar) {
-        if ($this->oscar === $oscar) {
-            $this->oscar = 'Academy Award–winning';
+        function __construct($title, $genre, $director, $starring, $date, $language, $oscar){
+            $this-> title= $title;
+            $this-> genre= $genre;
+            $this-> director= $director;
+            $this-> starring= $starring;
+            $this-> date= $date;
+            $this-> language= $language;
+            $this-> oscar= $oscar;
+        }    
+            
+        // funzione per attribuire il premio
+        public function setOscar ($oscar) {
+            if ($this->oscar === $oscar) {
+                $this->oscar = 'Academy Award-winning';
+            }
+            else {
+                $this->oscar = 'No Award-winning';
+            }
         }
-        else {
-            $this->oscar = 'No Award–winning';
-        }
-    }
 
-    public function getOscar(){
-        return $this->oscar;
-    }
+        public function getOscar(){
+            return $this->oscar;
+        }
 
 }
 
--- index.php
+++ index.php
@@ -1,41 +1,47 @@
 <?php
 
-    class Movie {
-        public $title;
-        public $genre;
-        public $director;
-        public $starring;
-        public $date;
-        public $language;
-        public $oscar;
+class Movie {
+    public $title;
+    public $genre;
+    public $director;
+    public $starring;
+    public $date;
+    public $language;
+    public $oscar;
+   
+
+    function __construct($title, $genre, $director, $starring, $date, $language, $oscar){
+        $this-> title= $title;
+        $this-> genre= $genre;
+        $this-> director= $director;
+        $this-> starring= $starring;
+        $this-> date= $date;
+        $this-> language= $language;
+        $this-> oscar= $oscar;
+    }    
         
-        // funzione per attribuire il premio
-        public function setOscar ($oscar) {
-            if ($this->oscar === $oscar) {
-                $this->oscar = 'Ha vinto un Oscar';
-            }
-            else {
-                $this->oscar = 'Non ha vinto un Oscar';
-            }
+    // funzione per attribuire il premio
+    public function setOscar ($oscar) {
+        if ($this->oscar === $oscar) {
+            $this->oscar = 'Academy Award-winning';
         }
-
-        public function getOscar(){
-            return $this->oscar;
+        else {
+            $this->oscar = 'No Award-winning';
         }
-
     }
+
+    public function getOscar(){
+        return $this->oscar;
+    }
+
+}
 
 
     // primo film
-    $alien = new Movie();
-    $alien->title ="Alien";
-    $alien->genre ="Science fiction";
-    $alien->director ="Ridley Scott";
-    $alien->starring =["Tom Skerritt","Sigourney Weaver","Veronica Cartwright","Harry Dean Stanton","Ian Holm","Yaphet Kotto","John Hurt"];
-    $alien->date = 1979;
-    $alien->language ="English";
-    $alien->oscar = true;
+    // primo film
+    $alien = new Movie('Alien','Science fiction','Ridley Scott',['Tom Skerritt','Sigourney Weaver','Veronica Cartwright','Ian Holm','John Hurt'], 1979,'English', true );
     $alien->setOscar(true);
+    
 
     echo $alien->title. "<br/>". $alien->genre. "<br/>". $alien->director. "<br/>".$alien->date. "<br/>".$alien->language. "<br/>".$alien->getOscar(). "<br/>";
 
@@ -45,15 +51,9 @@
 
 
     // secondo film
-    $tremors = new Movie();
-    $tremors->title ="Tremors";
-    $tremors->genre ="Horror";
-    $tremors->director ="Ron Underwood";
-    $tremors->starring =["Kevin Bacon", "Fred Ward","Finn Carter","Michael Gross","Reba McEntire",];
-    $tremors->date = 1990;
-    $tremors->language ="English";
-    $tremors->oscar = false;
+    $tremors = new Movie('Tremors','Horror','Ron Underwood',["Kevin Bacon", "Fred Ward","Finn Carter","Michael Gross","Reba McEntire"], 1990,'English', false );
     $tremors->setOscar(true);
+    
     
    
     echo "<br/>". "<br/>". "<br/>". $tremors->title. "<br/>". $tremors->genre. "<br/>". $tremors->director. "<br/>" .$tremors->date. "<br/>".$tremors->language. "<br/>".$tremors->getOscar(). "<br/>";
